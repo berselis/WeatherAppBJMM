74f9362454c58d52193d118b3c7f43ad3bf9af2b
Berselis Mendoza <berselismendoza@gmail.com>
2022-08-06 12:05:29 -0400

    Deploy
    update code

--- src/components/MainsBgObject.js
+++ src/components/MainsBgObject.js
@@ -3,20 +3,20 @@
 
 
 export const MainsBg = {
-    'Thunderstorm': 'src/assets/mainsbg/thunderstorm.png',
-    'Drizzle':'src/assets/mainsbg/drizzle.png',
-    'Rain': 'src/assets/mainsbg/rain.png',
-    'Snow': 'src/assets/mainsbg/snow.png',
-    'Mist': 'src/assets/mainsbg/mist.png',
-    'Smoke': 'src/assets/mainsbg/smoke.png',
-    'Haze': 'src/assets/mainsbg/haze.png',
-    'Dust': 'src/assets/mainsbg/dust.png',
-    'Fog': 'src/assets/mainsbg/fog.png',
-    'Sand': 'src/assets/mainsbg/sand.png',
-    'Ash': 'src/assets/mainsbg/ash.png',
-    'Squall': 'src/assets/mainsbg/squall.png',
-    'Tornado': 'src/assets/mainsbg/tornado.png',
-    'Clear': 'src/assets/mainsbg/clear.png',
-    'Clouds': 'src/assets/mainsbg/clouds.png'
+    'Thunderstorm': 'https://drive.google.com/file/d/1-_MukIjk3R5-zbz2sIvfqXo5pkjrMWoB/view?usp=sharing',
+    'Drizzle':'https://drive.google.com/file/d/10-kFWz2yRS2MpnO_Tz75RAp8fvI8Hxo4/view?usp=sharing',
+    'Rain': 'https://drive.google.com/file/d/1-loj6AZNUpuco8xT5QY_p_jNlSnIl2_0/view?usp=sharing',
+    'Snow': 'https://drive.google.com/file/d/1-jakr7N4rNPIdngtv2Inig475g0E2sjw/view?usp=sharing',
+    'Mist': 'https://drive.google.com/file/d/1-luJ_TcFbLrmaZ_Aih1a_jZ6bKroke27/view?usp=sharing',
+    'Smoke': 'https://drive.google.com/file/d/1-jns7Ws7zoxJwuh04lmKZiBvXvnS6oGm/view?usp=sharing',
+    'Haze': 'https://drive.google.com/file/d/1-qew-f52gHkTYIz9S7BmDDnvS_yVMPd9/view?usp=sharing',
+    'Dust': 'https://drive.google.com/file/d/1-sRFAHgujmDA0a1Xr1NGZ_rgudT5jK0u/view?usp=sharing',
+    'Fog': 'https://drive.google.com/file/d/1-rfJYXmMmQH7fnwf5OV2a-q44ifFEir8/view?usp=sharing',
+    'Sand': 'https://drive.google.com/file/d/1-kPU0G5XstyK7h3xGSdQA5smZ8lgEeyd/view?usp=sharing',
+    'Ash': 'https://drive.google.com/file/d/10IXPPZs0VnaBOBRvKHDdUQ9bPWJfC8yf/view?usp=sharing',
+    'Squall': 'https://drive.google.com/file/d/1-hOcUbECFbGgKq7gmljcbq19k4U0Qw69/view?usp=sharing',
+    'Tornado': 'https://drive.google.com/file/d/1-VuM02H5RpTmViKEQ68qsCfJiCmrq6lP/view?usp=sharing',
+    'Clear': 'https://drive.google.com/file/d/10Gy-18hm4BNSFDILcedoTmXeIqIQD59x/view?usp=sharing',
+    'Clouds': 'https://drive.google.com/file/d/1067lEiFnLW6Ob-mtIL_7p50Ep-NRW9Lo/view?usp=sharing'
 
 }
