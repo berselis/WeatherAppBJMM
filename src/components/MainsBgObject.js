babc36a59965b4cf1c0886ccec296efbba2f95e2
Berselis Mendoza <berselismendoza@gmail.com>
2022-08-07 11:40:03 -0400

    Deploy
    update code

--- src/components/MainsBgObject.js
+++ src/components/MainsBgObject.js
@@ -3,20 +3,20 @@
 
 
 export const MainsBg = {
-    'Thunderstorm': 'https://drive.google.com/file/d/1-_MukIjk3R5-zbz2sIvfqXo5pkjrMWoB/view?usp=sharing',
-    'Drizzle':'https://drive.google.com/file/d/10-kFWz2yRS2MpnO_Tz75RAp8fvI8Hxo4/view?usp=sharing',
-    'Rain': 'https://drive.google.com/file/d/1-loj6AZNUpuco8xT5QY_p_jNlSnIl2_0/view?usp=sharing',
-    'Snow': 'https://drive.google.com/file/d/1-jakr7N4rNPIdngtv2Inig475g0E2sjw/view?usp=sharing',
-    'Mist': 'https://drive.google.com/file/d/1-luJ_TcFbLrmaZ_Aih1a_jZ6bKroke27/view?usp=sharing',
-    'Smoke': 'https://drive.google.com/file/d/1-jns7Ws7zoxJwuh04lmKZiBvXvnS6oGm/view?usp=sharing',
-    'Haze': 'https://drive.google.com/file/d/1-qew-f52gHkTYIz9S7BmDDnvS_yVMPd9/view?usp=sharing',
-    'Dust': 'https://drive.google.com/file/d/1-sRFAHgujmDA0a1Xr1NGZ_rgudT5jK0u/view?usp=sharing',
-    'Fog': 'https://drive.google.com/file/d/1-rfJYXmMmQH7fnwf5OV2a-q44ifFEir8/view?usp=sharing',
-    'Sand': 'https://drive.google.com/file/d/1-kPU0G5XstyK7h3xGSdQA5smZ8lgEeyd/view?usp=sharing',
-    'Ash': 'https://drive.google.com/file/d/10IXPPZs0VnaBOBRvKHDdUQ9bPWJfC8yf/view?usp=sharing',
-    'Squall': 'https://drive.google.com/file/d/1-hOcUbECFbGgKq7gmljcbq19k4U0Qw69/view?usp=sharing',
-    'Tornado': 'https://drive.google.com/file/d/1-VuM02H5RpTmViKEQ68qsCfJiCmrq6lP/view?usp=sharing',
-    'Clear': 'https://drive.google.com/file/d/10Gy-18hm4BNSFDILcedoTmXeIqIQD59x/view?usp=sharing',
-    'Clouds': 'https://drive.google.com/file/d/1067lEiFnLW6Ob-mtIL_7p50Ep-NRW9Lo/view?usp=sharing'
+    'Thunderstorm': 'https://bdevelopment.net/resourse/img/weatherbgapp/Thunderstorm.png',
+    'Drizzle':'https://bdevelopment.net/resourse/img/weatherbgapp/Drizzle.png',
+    'Rain': 'https://bdevelopment.net/resourse/img/weatherbgapp/Rain.png',
+    'Snow': 'https://bdevelopment.net/resourse/img/weatherbgapp/Snow.png',
+    'Mist': 'https://bdevelopment.net/resourse/img/weatherbgapp/Mist.png',
+    'Smoke': 'https://bdevelopment.net/resourse/img/weatherbgapp/Smoke.png',
+    'Haze': 'https://bdevelopment.net/resourse/img/weatherbgapp/Haze.png',
+    'Dust': 'https://bdevelopment.net/resourse/img/weatherbgapp/Dust.png',
+    'Fog': 'https://bdevelopment.net/resourse/img/weatherbgapp/Fog.png',
+    'Sand': 'https://bdevelopment.net/resourse/img/weatherbgapp/Sand.png',
+    'Ash': 'https://bdevelopment.net/resourse/img/weatherbgapp/Ash.png',
+    'Squall': 'https://bdevelopment.net/resourse/img/weatherbgapp/Squall.png',
+    'Tornado': 'https://bdevelopment.net/resourse/img/weatherbgapp/Tornado.png',
+    'Clear': 'https://bdevelopment.net/resourse/img/weatherbgapp/Clear.png',
+    'Clouds': 'https://bdevelopment.net/resourse/img/weatherbgapp/Clouds.png'
 
 }
